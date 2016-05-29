0f528b2a0edefa27c1721c92a80c3756f2143ca2
cjp <No5stranger@gmail.com>
2016-05-29 23:28:53 +0800

    thread lock example

---
+++ thread_lock.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import time
+from threading import Thread, Lock
+
+
+class Counter(object):
+    def __init__(self):
+        self.lock = Lock()
+        self.count = 0
+
+    def increment(self, offset=1):
+        self.count += offset
+
+    def increment_with_lock(self, offset=1):
+        with self.lock:
+            self.count += offset
+
+
+def worker(sensor_index, how_many, counter):
+    for _ in range(how_many):
+        # counter.increment()
+        counter.increment_with_lock()
+
+
+def run_threads(func, how_many, counter):
+    threads = []
+    for i in range(5):
+        args = (i, how_many, counter)
+        thread = Thread(target=func, args=args)
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+start = time.time()
+how_many = 10**5
+counter = Counter()
+run_threads(worker, how_many, counter)
+end = time.time()
+time_cost = end - start
+print('Counter should be %d, found %d' % (5 * how_many, counter.count))
+print('Time cost: %.8f' % time_cost)
